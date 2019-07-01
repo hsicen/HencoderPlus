c94a5e669083b2dc1fed08b31fc533a23e9e22ed
Night <codinghuang@163.com>
2019-07-01 15:25:57 +0800

    自定义View颜色和位置渐变

--- drawing/src/main/java/com/hsicen/drawing/MainActivity.kt
+++ drawing/src/main/java/com/hsicen/drawing/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         btnStart.setOnClickListener {
 
-            sportView.startProgress()
+            sportView.startAnimate()
         }
     }
 }
