d6790d6b06512185558c95ad805c654c9e81f207
hsicen <codinghuang@163.com>
2019-07-25 10:54:31 +0800

    自定义Drawable进行图形绘制

--- drawable/src/main/java/com/hsicen/drawable/MeshDrawable.kt
+++ drawable/src/main/java/com/hsicen/drawable/MeshDrawable.kt
@@ -1,22 +1,44 @@
 package com.hsicen.drawable
 
-import android.graphics.Canvas
-import android.graphics.ColorFilter
-import android.graphics.Paint
-import android.graphics.PixelFormat
+import android.graphics.*
 import android.graphics.drawable.Drawable
 
 /**
  * <p>作者：Hsicen  2019/7/22 16:36
  * <p>邮箱：codinghuang@163.com
  * <p>作用：
- * <p>描述：HencoderPlus
+ * <p>描述：自定义Drawable可用于自定义View中的公共部分
+ *
+ * Drawable也是使用Canvas来进行绘制的
  */
 class MeshDrawable : Drawable() {
     private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val mLines = 20
 
     override fun draw(canvas: Canvas) {
+        mPaint.color = Color.parseColor("#0080FF")
+        mPaint.strokeWidth = 3f.dp2px
 
+        val widthGap = bounds.width() / mLines * 1f
+        val heightGap = bounds.height() / mLines * 1f
+
+        for (index in 0..mLines) {
+            canvas.drawLine(
+                index * widthGap,
+                bounds.top.toFloat(),
+                index * widthGap,
+                bounds.bottom.toFloat(),
+                mPaint
+            )
+
+            canvas.drawLine(
+                bounds.left.toFloat(),
+                index * heightGap,
+                bounds.right.toFloat(),
+                index * heightGap,
+                mPaint
+            )
+        }
     }
 
     override fun setAlpha(alpha: Int) {
@@ -27,9 +49,9 @@ class MeshDrawable : Drawable() {
 
     override fun getOpacity(): Int {
         return when (mPaint.alpha) {
-            0XFF -> PixelFormat.TRANSPARENT
-            0 -> PixelFormat.TRANSLUCENT
-            else -> PixelFormat.UNKNOWN
+            0XFF -> PixelFormat.OPAQUE
+            0 -> PixelFormat.TRANSPARENT
+            else -> PixelFormat.TRANSLUCENT
         }
     }
 
