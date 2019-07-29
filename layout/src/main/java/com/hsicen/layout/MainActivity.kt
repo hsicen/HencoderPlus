2db05f201c1825038d0af79d75eec1bc01ccbe75
Hsicen <hsiceni@gmail.com>
2019-07-29 08:10:56 +0800

    布局流程

---
+++ layout/src/main/java/com/hsicen/layout/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hsicen.layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <p>作者：Hsicen  19-7-28 下午4:13
+ * <p>邮箱：codinghuang@163.com
+ * <p>功能：
+ * <p>描述：自定义布局
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
