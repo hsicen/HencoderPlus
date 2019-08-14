9a4c62ea65cb7fb0d6cc3650836e3bd52b7c147a
hsicen <codinghuang@163.com>
2019-08-14 08:11:14 +0800

    Motion Layout 动画

---
+++ 20_motionlayout/src/main/java/com/hsicen/a20_motionlayout/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.hsicen.a20_motionlayout
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_scene_start.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scene_start)
+
+
+        btnStart.setOnClickListener {
+            ml.transitionToStart()
+        }
+
+        btnEnd.setOnClickListener {
+            ml.transitionToEnd()
+        }
+
+        btnMenu.setOnClickListener {
+            startActivity(
+                Intent(this, MotionMenuActivity::class.java)
+            )
+        }
+
+        ml.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+
+            }
+
+        })
+    }
+}
---
+++ 20_motionlayout/src/main/java/com/hsicen/a20_motionlayout/MotionMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.hsicen.a20_motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MotionMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_start)
+    }
+}
