ebc7b601699a66d00b955d6450a1814a7677a2b0
hsicen <codinghuang@163.com>
2019-08-19 15:06:27 +0800

    MultiTouchView 多指触控协作型处理

--- 21_multitouch/src/main/java/com/example/a21_multitouch/view/MultiTouchView.kt
+++ 21_multitouch/src/main/java/com/example/a21_multitouch/view/MultiTouchView.kt
@@ -12,7 +12,7 @@ import android.view.View
  * <p>作者：Hsicen  19-8-18 下午7:25
  * <p>邮箱：codinghuang@163.com
  * <p>功能：
- * <p>描述： 多点触控
+ * <p>描述： 多点触控(协作型)
  */
 class MultiTouchView @JvmOverloads constructor(
     context: Context,
@@ -29,29 +29,25 @@ class MultiTouchView @JvmOverloads constructor(
     private var imageX = 0f
     private var imageY = 0f
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val width = resolveSize(400, widthMeasureSpec)
-        val height = resolveSize(400, heightMeasureSpec)
-
-        setMeasuredDimension(width, height)
-    }
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         mPaint.color = Color.parseColor("#D79BE3")
-        canvas.drawRect(offsetX, offsetY, offsetX + 600f, offsetY + 600f, mPaint)
+        canvas.drawRect(offsetX, offsetY, offsetX + 500f, offsetY + 500f, mPaint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        val count = event.pointerCount
         var sumX = 0f
         var sumY = 0f
+        var count = event.pointerCount
+        val isPointerUp = event.actionMasked == MotionEvent.ACTION_POINTER_UP
         for (index in 0 until count) {
+            if (isPointerUp && event.actionIndex == index) continue
             sumX += event.getX(index)
             sumY += event.getY(index)
         }
 
+        if (isPointerUp) count--
         val focusX = sumX / count
         val focusY = sumY / count
 
@@ -59,6 +55,7 @@ class MultiTouchView @JvmOverloads constructor(
             MotionEvent.ACTION_POINTER_UP,
             MotionEvent.ACTION_POINTER_DOWN,
             MotionEvent.ACTION_DOWN -> {
+                //重置初始坐标
                 downX = focusX
                 downY = focusY
                 imageX = offsetX
@@ -66,6 +63,7 @@ class MultiTouchView @JvmOverloads constructor(
             }
 
             MotionEvent.ACTION_MOVE -> {
+                //更新坐标
                 offsetX = imageX + focusX - downX
                 offsetY = imageY + focusY - downY
                 invalidate()
