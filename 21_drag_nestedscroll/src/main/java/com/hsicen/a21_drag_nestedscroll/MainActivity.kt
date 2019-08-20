2fceef28c85169cb46cf9f7de60c66563dbbf152
hsicen <codinghuang@163.com>
2019-08-20 16:28:05 +0800

    课程目录代码整理

---
+++ 21_drag_nestedscroll/src/main/java/com/hsicen/a21_drag_nestedscroll/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hsicen.a21_drag_nestedscroll
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <p>作者：Hsicen  2019/8/20 16:00
+ * <p>邮箱：codinghuang@163.com
+ * <p>功能：
+ * <p>描述：View 拖拽和滑动冲突处理
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
