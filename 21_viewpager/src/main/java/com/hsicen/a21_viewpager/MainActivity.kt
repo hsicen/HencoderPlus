d93e9ad3067d727dd81645fe1e100641db7c9134
hsicen <codinghuang@163.com>
2019-08-20 15:46:29 +0800

    ViewGroup事件分发逻辑处理，添加相关注释说明

--- 21_viewpager/src/main/java/com/hsicen/a21_viewpager/MainActivity.kt
+++ 21_viewpager/src/main/java/com/hsicen/a21_viewpager/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hsicen.a21_viewpager
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.d("hsc", "点击")
+        return true
     }
 }
