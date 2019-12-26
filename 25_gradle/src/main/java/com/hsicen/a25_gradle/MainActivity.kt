7ecaf12dad58e39e9ab54cc64665b39d1a307910
hsicen <hsiceni@google.com>
2019-12-26 09:07:07 +0800

    自定义gradle plugin

---
+++ 25_gradle/src/main/java/com/hsicen/a25_gradle/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hsicen.a25_gradle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Utils.drawBadge(this)
+    }
+}
---
+++ buildSrc/src/main/groovy/com/hsicen/plugin/HsicenExtension.groovy
@@ -0,0 +1,5 @@
+package com.hsicen.plugin
+
+class HsicenExtension {
+    String name = 'Hsicen'
+}
