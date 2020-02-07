deab3be310e6a256c2db6e185a63854494f5ebb0
hsicen <hsiceni@google.com>
2020-02-07 18:27:52 +0800

    Android 热修复方案初始化

---
+++ 29_hotfix/src/main/java/com/hsicen/a29_hotfix/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.hsicen.a29_hotfix
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_load.setOnClickListener {
+
+
+        }
+
+        btn_hotfix.setOnClickListener {
+
+        }
+    }
+}
