757313f8cc510de80fc9b4b13dd5340b29c52fd6
hsicen <codinghuang@163.com>
2020-02-12 16:20:18 +0800

    自定义编译时注解库，实现ButterKnife绑定View的功能

--- 30_lib_processos/src/main/java/com/hsicen/a30_lib_processor/BindingProcessor.java
+++ 30_lib_processos/src/main/java/com/hsicen/a30_lib_processor/BindingProcessor.java
@@ -1,14 +1,82 @@
 package com.hsicen.a30_lib_processor;
 
+import com.hsicen.a30_lib_annotation.BindView;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 
 public class BindingProcessor extends AbstractProcessor {
-	@Override
-	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		return false;
-	}
+    private Filer mFiler;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+
+        mFiler = processingEnv.getFiler();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        for (Element element : roundEnv.getRootElements()) {
+            String pkgStr = element.getEnclosingElement().toString();  //包名
+            String classStr = element.getSimpleName().toString(); //类名
+
+            //构造自动生成的类名 (类名 + Binding)
+            ClassName className = ClassName.get(pkgStr, classStr + "Binding");
+            MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                    .addModifiers(Modifier.PUBLIC)
+                    .addParameter(ClassName.get(pkgStr, classStr), "activity");
+
+            boolean hasBind = false;
+            for (Element enclosedElement : element.getEnclosedElements()) {
+                if (enclosedElement.getKind() == ElementKind.FIELD) {
+                    BindView bindView = enclosedElement.getAnnotation(BindView.class);
+                    if (bindView != null) {
+                        hasBind = true;
+                        constructorBuilder.addStatement("activity.$N = activity.findViewById($L)",
+                                enclosedElement.getSimpleName(), bindView.value());
+                    }
+                }
+            }
+
+            //生成Class
+            TypeSpec buildClass = TypeSpec.classBuilder(className)
+                    .addModifiers(Modifier.PUBLIC)
+                    .addMethod(constructorBuilder.build())
+                    .build();
+
+            //写入到指定位置
+            if (hasBind) {
+                try {
+                    JavaFile.builder(pkgStr, buildClass)
+                            .build()
+                            .writeTo(mFiler);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(BindView.class.getCanonicalName());
+    }
 }
