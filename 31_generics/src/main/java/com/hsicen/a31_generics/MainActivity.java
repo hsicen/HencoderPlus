8dd14af5d678a851264c6ce38089f1354ed114da
hsicen <hsiceni@google.com>
2020-02-13 10:58:49 +0800

    泛型讲解

---
+++ 31_generics/src/main/java/com/hsicen/a31_generics/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hsicen.a31_generics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 作者：hsicen  2020/2/13 10:56
+ * 邮箱：codinghuang@163.com
+ * 功能：
+ * 描述：泛型讲解
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
