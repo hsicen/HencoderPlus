3493591dfb1b3aba8cd34eac479c7e7e620089e9
hsicen <codinghuang@163.com>
2020-04-08 16:06:08 +0800

    Kotlin 的使用，参考文档：Kotlin官方文档，开学IO文档

---
+++ 5_kotlin/src/main/java/com/hsicen/a5_kotlin/KotlinClass.kt
@@ -0,0 +1,13 @@
+package com.hsicen.a5_kotlin
+
+/**
+ * 作者：hsicen  2020/4/8 11:21
+ * 邮箱：codinghuang@163.com
+ * 功能：
+ * 描述：Kotlin 的使用
+ */
+class KotlinClass {
+    //Kotlin 官方文档   https://www.kotlincn.net/docs/reference/
+
+    //开学IO kotlin部分 https://kaixue.io/
+}
