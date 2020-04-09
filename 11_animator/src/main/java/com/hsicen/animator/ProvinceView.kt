adfd75c72dbeb6fdfff8c40646b664eda8af9e77
hsicen <codinghuang@163.com>
2020-04-09 17:54:10 +0800

    动画：     属性动画：Animator，ValueAnimator，ViewPropertyAnimator，ObjectAnimator，PropertyValueHolder     视图动画：Animation 估值器：计算动画在起始和结束范围之间的每个点的属性值 硬件加速：利用GPU进行绘制工作

--- 11_animator/src/main/java/com/hsicen/animator/ProvinceView.kt
+++ 11_animator/src/main/java/com/hsicen/animator/ProvinceView.kt
@@ -1,9 +1,7 @@
 package com.hsicen.animator
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -13,11 +11,27 @@ import android.view.View
  * <p>作用：
  * <p>描述：字符串动画
  */
-class ProvinceView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+class ProvinceView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
     View(context, attrs, defStyleAttr) {
 
     private val mPaint by lazy {
         Paint(Paint.ANTI_ALIAS_FLAG)
+    }
+
+    private val mMatrixFrom by lazy {
+        Matrix()
+    }
+
+    private val mMatrixTo by lazy {
+        Matrix()
+    }
+
+    private val mCamera by lazy {
+        Camera()
     }
 
     var mProvince = "北京市"
@@ -34,6 +48,14 @@ class ProvinceView @JvmOverloads constructor(context: Context, attrs: AttributeS
         mPaint.textAlign = Paint.Align.CENTER
 
         canvas.drawText(mProvince, width / 2f, height / 2f, mPaint)
+
+        mCamera.save()
+        canvas.save()
+        mCamera.translate(3f, 5f, 0f)
+        mCamera.rotateX(40f)
+        mCamera.applyToCanvas(canvas)
+        canvas.restore()
+        mCamera.restore()
     }
 
 }
