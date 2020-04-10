aaf4f80e0dc2124156f0c223b081a3cad33db3ce
hsicen <codinghuang@163.com>
2020-04-10 15:42:34 +0800

    事件分发(root->child)、拦截(root->child)、处理(child->root) Activity -> ViewGroup -> View

--- 14_touch/src/main/java/com/hsicen/touch/MainActivity.kt
+++ 14_touch/src/main/java/com/hsicen/touch/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.hsicen.touch
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mTouchDelegate by lazy {
+        GestureDetectorCompat(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent?) = true
+
+            override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+                Toast.makeText(this@MainActivity, "单击操作", Toast.LENGTH_SHORT).show()
+                return super.onSingleTapConfirmed(e)
+            }
+
+            override fun onDoubleTap(e: MotionEvent?): Boolean {
+                Toast.makeText(this@MainActivity, "双击操作", Toast.LENGTH_SHORT).show()
+                return super.onDoubleTap(e)
+            }
+        })
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +33,10 @@ class MainActivity : AppCompatActivity() {
 
         touchView.setOnClickListener {
             Toast.makeText(this, "被点击了", Toast.LENGTH_SHORT).show()
+        }
+
+        iv_touch_sample.setOnTouchListener { _, event ->
+            mTouchDelegate.onTouchEvent(event)
         }
     }
 }
