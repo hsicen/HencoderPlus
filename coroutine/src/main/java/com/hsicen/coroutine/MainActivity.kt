1b1dbb692a3daff3fcae14252c595061a06b0c03
hsicen <codinghuang@163.com>
2020-09-26 11:45:22 +0800

    项目SDK版本更新

--- coroutine/src/main/java/com/hsicen/coroutine/MainActivity.kt
+++ coroutine/src/main/java/com/hsicen/coroutine/MainActivity.kt
@@ -8,34 +8,34 @@ import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+  private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-        zipRequest()
+    zipRequest()
+  }
+
+  /*** 利用协程合并请求*/
+  @SuppressLint("SetTextI18n")
+  private fun zipRequest() {
+    GlobalScope.launch(Dispatchers.Main) {
+      val result1 = async { requestData1() }
+      val result2 = async { requestData2() }
+
+      binding.tvInfo.text = "${result1.await()} + ${result2.await()}"
     }
+  }
 
-    /*** 利用协程合并请求*/
-    @SuppressLint("SetTextI18n")
-    private fun zipRequest() {
-        GlobalScope.launch(Dispatchers.Main) {
-            val result1 = async { requestData1() }
-            val result2 = async { requestData2() }
+  private suspend fun requestData1() = withContext(Dispatchers.IO) {
+    delay(3000L)
+    "hsicen"
+  }
 
-            binding.tvInfo.text = "${result1.await()} + ${result2.await()}"
-        }
-    }
-
-    private suspend fun requestData1() = withContext(Dispatchers.IO) {
-        delay(3000L)
-        "hsicen"
-    }
-
-    private suspend fun requestData2() = withContext(Dispatchers.IO) {
-        delay(1500)
-        "miky"
-    }
+  private suspend fun requestData2() = withContext(Dispatchers.IO) {
+    delay(1500)
+    "miky"
+  }
 }
