0a79458501f652f92314d86b1b5dc8d4a899f0fe
hsicen <codinghuang@163.com>
2021-05-05 21:22:26 +0800

    协程框架探索

---
+++ coroutine/src/main/java/com/hsicen/coroutine/lite.kt
@@ -0,0 +1,58 @@
+package com.hsicen.coroutine
+
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+/**
+ * 作者：hsicen  5/5/21 8:13 PM
+ * 邮箱：codinghuang@163.com
+ * 作用：
+ * 描述：HencoderPlus
+ */
+
+//1.实现一个delay函数
+private val executor = Executors.newScheduledThreadPool(1) { runnable ->
+    Thread(runnable, "Scheduler").apply { isDaemon = true }
+}
+
+suspend fun delay(time: Long, unit: TimeUnit = TimeUnit.MILLISECONDS) {
+    if (time <= 0) {
+        return
+    } else {
+        suspendCoroutine<Unit> { continuation ->
+            executor.schedule({ continuation.resume(Unit) }, time, unit)
+        }
+    }
+}
+
+//2.协程的描述类
+typealias OnComplete = () -> Unit
+typealias OnCancel = () -> Unit
+
+
+interface Job : CoroutineContext.Element {
+    companion object Key : CoroutineContext.Key<Job>
+
+    override val key: CoroutineContext.Key<*> get() = Job
+
+    val isActive: Boolean
+
+    //fun invokeOnCancel(onCancel: OnCancel): Disposable
+
+    //fun invokeOnComplete(onComplete: OnComplete): Disposable
+
+    fun cancel()
+
+    //fun remove(disposable: Disposable)
+
+    suspend fun join()
+}
+
+sealed class CoroutineState {
+    class Incomplete : CoroutineState()
+    class Cancelling : CoroutineState()
+    class Complete<T>(val value: T? = null, val exception: Throwable? = null) : CoroutineState()
+}
---
+++ coroutine/src/main/java/com/hsicen/coroutine/practice.kt
@@ -0,0 +1,38 @@
+package com.hsicen.coroutine
+
+/**
+ * 作者：hsicen  5/5/21 6:02 PM
+ * 邮箱：codinghuang@163.com
+ * 作用：
+ * 描述：HencoderPlus
+ */
+
+
+val sequence = sequence {
+    yield(1)
+    yield(2)
+    yield(3)
+    yield(4)
+    yieldAll(listOf(1, 2, 3, 4, 5))
+}
+
+val fibonacci = sequence {
+    yield(1L)   //first number
+    var cur = 1L
+    var next = 1L
+
+    while (true) {
+        yield(next) //next number
+
+        next += cur //next = cur + next
+        cur = next - cur //cur = next
+    }
+}
+
+fun main() {
+    for (item in sequence) {
+        println(item)
+    }
+
+    fibonacci.take(10).forEach(::println)
+}
