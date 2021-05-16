42a22e2aa2584f20730f6c7f1da7614ffb58f560
hsicen <codinghuang@163.com>
2021-05-16 16:46:41 +0800

    项目整理，kts脚本构建

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    `kotlin-dsl`
+  `kotlin-dsl`
 }
 
 repositories {
-    google()
-    mavenCentral()
+  google()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.2.1")
+  implementation("com.android.tools.build:gradle:4.2.1")
 }
