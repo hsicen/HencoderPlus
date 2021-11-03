4872125cf8fe44b203bf2e0e9eea4093dd9924c5
hsicen <codinghuang@163.com>
2021-11-03 15:24:50 +0800

    update app-module comm configuration.

--- 28_plugin_lib/build.gradle.kts
+++ 28_plugin_lib/build.gradle.kts
@@ -1,41 +1,39 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+  id("comm.app-module")
 }
 
 android {
-    compileSdk = Versions.compileSdk
 
-    defaultConfig {
-        applicationId = "com.hsicen.a28_plugin_lib"
-        minSdk = Versions.minSdk
-        targetSdk = Versions.targetSdk
-        versionCode = Versions.versionCode
-        versionName = Versions.versionName
+  defaultConfig {
+    applicationId = "com.hsicen.a28_plugin_lib"
+    minSdk = Versions.minSdk
+    targetSdk = Versions.targetSdk
+    versionCode = Versions.versionCode
+    versionName = Versions.versionName
 
-        testInstrumentationRunner = TestDeps.runner
+    testInstrumentationRunner = TestDeps.runner
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+  }
 }
 
 dependencies {
-    testImplementation(TestDeps.junit)
-    androidTestImplementation(TestDeps.junitExt)
-    androidTestImplementation(TestDeps.espresso)
+  testImplementation(TestDeps.junit)
+  androidTestImplementation(TestDeps.junitExt)
+  androidTestImplementation(TestDeps.espresso)
 
-    implementation(fileTree(Deps.fileMap))
-    implementation(Deps.kotlinStb)
+  implementation(fileTree(Deps.fileMap))
+  implementation(Deps.kotlinStb)
 
-    implementation(Deps.appCompat)
-    implementation(Deps.ktx)
-    implementation(Deps.constrainLayout)
+  implementation(Deps.appCompat)
+  implementation(Deps.ktx)
+  implementation(Deps.constrainLayout)
 }
