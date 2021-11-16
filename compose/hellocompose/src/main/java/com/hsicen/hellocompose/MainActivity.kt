7bb8ab11bc69742b29730a9311c969522d284c07
hsicen <codinghuang@163.com>
2021-11-16 08:00:11 +0800

    hello compose project.

--- compose/hellocompose/src/main/java/com/hsicen/hellocompose/MainActivity.kt
+++ compose/hellocompose/src/main/java/com/hsicen/hellocompose/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,21 +29,49 @@ class MainActivity : ComponentActivity() {
       }
     }
   }
-}
 
-@Composable
-private fun TabItem(@DrawableRes iconId: Int, title: String, tint: Color) {
-  Column(Modifier.padding(16.dp)) {
-    Icon(painterResource(iconId), title, Modifier.size(24.dp), tint)
-    Text(text = title, fontSize = 11.sp, color = tint)
+  @Composable
+  fun TabItem(@DrawableRes iconId: Int, title: String, tint: Color, modifier: Modifier = Modifier) {
+    Column(
+      modifier.padding(vertical = 8.dp),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Icon(painterResource(iconId), title, Modifier.size(24.dp), tint)
+      Text(text = title, fontSize = 11.sp, color = tint)
+    }
   }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewTabItem() {
-  Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
-    TabItem(iconId = R.drawable.ic_chat_filled, title = "聊天", tint = Color.Green)
-    TabItem(iconId = R.drawable.ic_discover_filled, title = "朋友圈", tint = Color.Gray)
+
+  @Composable
+  fun WeBottomBar(select: Int) {
+    Row {
+      TabItem(selectIcon(0, select), "聊天", selectColor(0 == select), Modifier.weight(1f))
+      TabItem(selectIcon(1, select), "通讯录", selectColor(1 == select), Modifier.weight(1f))
+      TabItem(selectIcon(2, select), "发现", selectColor(2 == select), Modifier.weight(1f))
+      TabItem(selectIcon(3, select), "我", selectColor(3 == select), Modifier.weight(1f))
+    }
+  }
+
+  private fun selectIcon(pos: Int, select: Int): Int {
+    return when (pos) {
+      0 -> if (0 == select) R.drawable.ic_chat_filled else R.drawable.ic_chat_outlined
+      1 -> if (1 == select) R.drawable.ic_contacts_filled else R.drawable.ic_contacts_outlined
+      2 -> if (2 == select) R.drawable.ic_discover_filled else R.drawable.ic_discover_outlined
+      else -> if (3 == select) R.drawable.ic_me_filled else R.drawable.ic_me_outlined
+    }
+  }
+
+  private fun selectColor(select: Boolean) = if (select) Color.Green else Color.Black
+
+  @Preview(showBackground = true)
+  @Composable
+  fun PreviewTabItem() {
+    TabItem(selectIcon(2, 0), "聊天", selectColor(false))
+  }
+
+  @Preview(showBackground = true)
+  @Composable
+  fun PreviewBottomBar() {
+    WeBottomBar(1)
   }
 }
