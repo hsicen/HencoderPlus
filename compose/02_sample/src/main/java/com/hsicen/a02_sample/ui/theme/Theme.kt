bb800204c47c0b751ae2e1aaa3b5269ed46b767c
hsicen <codinghuang@163.com>
2022-02-25 22:58:04 +0800

    update.

--- compose/02_sample/src/main/java/com/hsicen/a02_sample/ui/theme/Theme.kt
+++ compose/02_sample/src/main/java/com/hsicen/a02_sample/ui/theme/Theme.kt
@@ -7,38 +7,29 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  primary = Purple200,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-      background = Color.White,
-      surface = Color.White,
-      onPrimary = Color.White,
-      onSecondary = Color.Black,
-      onBackground = Color.Black,
-      onSurface = Color.Black,
-      */
+  primary = Purple500,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 @Composable
 fun HencoderPlusTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+  MaterialTheme(
+    colors = colors,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
 }
