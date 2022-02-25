7da52cafd8c7e16061ddacf9fcf74cd48fd9890e
hsicen <codinghuang@163.com>
2022-02-25 22:08:11 +0800

    compose 基础控件.

--- compose/02_sample/src/main/java/com/hsicen/a02_sample/MainActivity.kt
+++ compose/02_sample/src/main/java/com/hsicen/a02_sample/MainActivity.kt
@@ -26,74 +26,74 @@ import coil.compose.rememberAsyncImagePainter
  * 描述：compose sample.
  */
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        setContent {
-            Column(Modifier.padding(16.dp)) {
-                Image(
-                    rememberAsyncImagePainter("https://cdn.pixabay.com/photo/2015/05/28/18/50/the-three-gorges-788314__340.jpg"),
-                    "Coil",
-                    modifier = Modifier.wrapContentSize()
-                )
-                Text(text = "Hello", fontSize = 40.sp, color = Color.Green)
-                Text(text = "Hello World", fontSize = 20.sp, color = Color.Red)
+    setContent {
+      Column(Modifier.padding(16.dp)) {
+        Image(
+          rememberAsyncImagePainter("https://cdn.pixabay.com/photo/2015/05/28/18/50/the-three-gorges-788314__340.jpg"),
+          "Coil",
+          modifier = Modifier.wrapContentSize()
+        )
+        Text(text = "Hello", fontSize = 40.sp, color = Color.Green)
+        Text(text = "Hello World", fontSize = 20.sp, color = Color.Red)
 
-                val names = listOf("Java", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift")
-                LazyColumn {
-                    items(names) { item ->
-                        Text(text = item)
-                        Spacer(modifier = Modifier.size(1.dp))
-                    }
+        val names = listOf("Java", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift", "Kotlin", "Flutter", "Swift")
+        LazyColumn {
+          items(names) { item ->
+            Text(text = item)
+            Spacer(modifier = Modifier.size(1.dp))
+          }
 
-                    item {
-                        Spacer(modifier = Modifier.size(8.dp))
-                        Image(
-                            rememberAsyncImagePainter("https://cdn.pixabay.com/photo/2015/05/28/18/50/the-three-gorges-788314__340.jpg"),
-                            "Coil",
-                            contentScale = ContentScale.FillBounds,
-                            modifier = Modifier
-                                .size(width = 320.dp, height = 180.dp)
-                                .clip(RoundedCornerShape(8.dp))
-                                .clickable {
+          item {
+            Spacer(modifier = Modifier.size(8.dp))
+            Image(
+              rememberAsyncImagePainter("https://cdn.pixabay.com/photo/2015/05/28/18/50/the-three-gorges-788314__340.jpg"),
+              "Coil",
+              contentScale = ContentScale.FillBounds,
+              modifier = Modifier
+                .size(width = 320.dp, height = 180.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .clickable {
 
-                                }
-                        )
-                        Spacer(modifier = Modifier.size(8.dp))
-                    }
-
-                    items(names) { item ->
-                        Text(
-                            text = item,
-                            Modifier
-                                .background(Color.Green)
-                                .padding(8.dp)
-                                .background(Color.Magenta)
-                                .padding(8.dp)
-                        )
-                        Spacer(modifier = Modifier.size(1.dp))
-                    }
-
-                    item {
-                        Button(onClick = { }) {
-                            Text(text = "智力 +10")
-                        }
-                        Spacer(modifier = Modifier.size(8.dp))
-
-                        OutlinedButton(onClick = {}) {
-                            Text(text = "力量 +10")
-                        }
-
-                        TextButton(onClick = {}) {
-                            Text(text = "攻击 +10")
-                        }
-
-                        FloatingActionButton(onClick = {}) {
-                            Text(text = "智力 +100")
-                        }
-                    }
                 }
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+          }
+
+          items(names) { item ->
+            Text(
+              text = item,
+              Modifier
+                .background(Color.Green)
+                .padding(8.dp)
+                .background(Color.Magenta)
+                .padding(8.dp)
+            )
+            Spacer(modifier = Modifier.size(1.dp))
+          }
+
+          item {
+            Button(onClick = { }) {
+              Text(text = "智力 +10")
             }
+            Spacer(modifier = Modifier.size(8.dp))
+
+            OutlinedButton(onClick = {}) {
+              Text(text = "力量 +10")
+            }
+
+            TextButton(onClick = {}) {
+              Text(text = "攻击 +10")
+            }
+
+            FloatingActionButton(onClick = {}) {
+              Text(text = "智力 +100")
+            }
+          }
         }
+      }
     }
+  }
 }
