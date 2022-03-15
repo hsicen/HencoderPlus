010420f1b47ec38b27c26b18702bad98c25cb39a
hsicen <codinghuang@163.com>
2022-03-15 18:14:47 +0800

    The subscribe process of mutableState.

--- compose/03_state/src/main/java/com/hsicen/state/MainActivity.kt
+++ compose/03_state/src/main/java/com/hsicen/state/MainActivity.kt
@@ -26,15 +26,6 @@ import kotlin.reflect.KProperty
  * 功能：
  * 描述：State
  *
- * 自定义Composable
- *  1. compose 编译器插件 (compiler plugin)，直接干预编译过程，对函数进行修改 (可以跨平台)
- *  2. 面向切面编程 (AOP): AnnotationProcessor/修改字节码 (只能用于JVM)
- *  3. Compose编译器插件 @Composable -> 识别符
- *  4. 自定义Composable = 自定义View/Xml布局文件 ?
- *     xml 是标记语言，没有逻辑能力
- *     等价物 -> xml布局文件 + 自定义View
- *
- *
  * 刷新：组合(Composition)、布局、绘制 -> Compose、布局、绘制
  * 组合过程：拼凑出界面实际内容
  * Compose/Composition -> ComposeView -> AndroidComposeView -> LayoutNode
@@ -42,23 +33,33 @@ import kotlin.reflect.KProperty
  * 过程：组合 Compose Composition
  * 结果：组合 Composition
  *
- * MutableState -> StateObject -> StateRecord -> Compose 支持事务功能
+ * MutableState -> StateObject -> StateRecord -> Compose  支持事务功能
  * 同一个变量存多个值，支持取消更新
- * 链表保存( StateRecord ) firstStateRecord(next) 为头结点
+ * 链表保存(StateRecord), StateObject.firstStateRecord 为头结点, StateRecord.next 保存下一个结点
+ *
+ *
+ * SnapshotMutableStateImpl.next(StateStateRecord)  为链表的头结点
+ * next.readable()/get函数 拿到链表头
+ *  readObserver 记录变量在哪些地方被读了
+ *  readable三个参数版本：遍历 StateRecord 链表，找到一个最新的、可用的 StateRecord
+ *  将 StateRecord 记录并返回给外部调用者
  *
  * Recompose 重组
- *
- * next.readable()/get函数  三个参数版本：遍历 StateRecord 链表，找到一个最新的、可用的 StateRecord
  * next.overwritable()/set函数
- * MutableState 的 value 被 get 时，使用 Snapshot.readObserver 进行记录
- *  set 时，使用 Snapshot.writeObserver 进行记录
+ *  withCurrent -> current -> readable 遍历 StateRecord 链表，找到一个最新的、可用的 StateRecord
+ *  将 StateRecord 和当前 set 的值比较是否相同
+ *  overwritable -> overwritableRecord -> 返回一个可用的 StateRecord
+ *    1.snapshotId 相同，直接返回该 StateRecord
+ *    2.不相同，newOverwritableRecord() 创建一个
+ *  writeObserver 将 readObserver 中记录标记为无效，在下一帧来临时进行刷新
  *
  * StateRecord：变量
  * Snapshot：整个状态；可以对应多个 StateRecord；一个 StateRecord 对应一个 Snapshot
+ *  多线程同步，批量更新支持
+ *  1.系统有多个 Snapshot 的时候，它们是有先后关系的
+ *  2.同一个 StateObject 的每个 StateRecord 都有它们对应的 Snapshot 的 id.
+ *    StateRecord 和 Snapshot 就算不直接对应，只要 StateRecord 的 Snapshot 对另一个是有效的，另一个就能读取到这个 StateRecord。
  *
- * 1.系统有多个 Snapshot 的时候，它们是有先后关系的
- * 2.同一个 StateObject 的每个 StateRecord 都有它们对应的 Snapshot 的 id.
- *   StateRecord 和 Snapshot 就算不直接对应，只要 StateRecord 的 Snapshot 对另一个是有效的，另一个就能读取到这个 StateRecord。
  *
  * 两个订阅过程：
  *  1.对 Snapshot 中读/写 StateObject 对象的订阅，分别订阅读和写，所以有两个接收者：readObserver 和 writeObserver
@@ -68,7 +69,8 @@ import kotlin.reflect.KProperty
  *  2.对【每一个】StateObject 的应用做订阅
  *      发生时间：
  *          订阅：第一个订阅的 readObserver 被调用（通知）的时候
- *          通知：StateObject 新值被应用的时候
+ *          通知：StateObject 新值被应用的时候 Recompose
+ *
  *
  * SnapshotMutableStateImpl.value
  *  get()：记录读 -> 订阅行为
@@ -89,16 +91,39 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContent {
-      MainScreen()
+    stateScreen1()
+  }
+
+
+  /**
+   * 拼凑出界面实际内容
+   */
+  private fun stateScreen1() {
+    val name = mutableStateOf("hsicen") // MutableState<T>
+
+    setContent { 
+      Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+      ) {
+        Text(name.value)
+      }
+    }
+
+    lifecycleScope.launch {
+      delay(3000)
+      name.value = "黄思程~~~"
     }
   }
 
-  @Composable
-  fun StateScreen() {
+  // by 代理实现
+  private fun stateScreen2() {
     var name by mutableStateOf("hsicen")
-    Button(modifier = Modifier.fillMaxSize(), onClick = {}) {
-      Text(name, textAlign = TextAlign.Center, fontSize = 24.sp)
+
+    setContent {
+      Button(modifier = Modifier.fillMaxSize(), onClick = {}) {
+        Text(name, textAlign = TextAlign.Center, fontSize = 24.sp)
+      }
     }
 
     lifecycleScope.launch {
@@ -107,6 +132,15 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  /**
+   * 自定义Composable
+   *  1. compose 编译器插件 (compiler plugin)，直接干预编译过程，对函数进行修改 (可以跨平台)
+   *  2. 面向切面编程 (AOP): AnnotationProcessor/修改字节码 (只能用于JVM)
+   *  3. Compose编译器插件 @Composable -> 识别符
+   *  4. 自定义Composable = 自定义View/Xml布局文件 ?
+   *     xml 是标记语言，没有逻辑能力
+   *     等价物 -> xml布局文件 + 自定义View
+   */
   @Composable
   fun MainScreen() {
     Box(
