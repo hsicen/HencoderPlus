3f6b9910b45786a5b318519791318a06c9a1c5f2
hsicen <codinghuang@163.com>
2023-07-25 17:32:27 +0800

    [Compose-46] SemanticsModifier 的原理。

--- compose/05_modifier/src/main/java/com/hsicen/a05_modifier/Chapter46.kt
+++ compose/05_modifier/src/main/java/com/hsicen/a05_modifier/Chapter46.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
@@ -25,7 +27,7 @@ import androidx.compose.ui.unit.dp
  *
  * 写法：
  *  1.Modifier.semantics(mergeDescendants: Boolean) { xxx = xxx }
- *    (1).mergeDescendants ：是否把内部的组件合并到⾃⼰这⾥来。
+ *    (1).mergeDescendants ：是否把内部的组件合并到⾃⼰这⾥来，同时自己不再被外部组件合并。
  *  2.Modifier.clearAndSetSemantics {} ：把⾃⼰内部的组件直接吞掉。
  *
  * 原理：
@@ -42,18 +44,57 @@ fun ComponentActivity.composeModifier08() {
 @Composable
 private fun ModifierSemantics01() {
   Column(modifier = Modifier.padding(16.dp)) {
-    Text(text = "床前明月光，疑是地上霜；", fontWeight = FontWeight(800))
+    Button(onClick = { /*TODO*/ }) {
+      Text(text = "静夜思", fontWeight = FontWeight(800),
+        modifier = Modifier.semantics(true) {
+
+        })
+    }
+
+    Text(text = "床前明月光，疑是地上霜；")
     Text(text = "举头望明月，低头思故乡。")
 
     Box(
       modifier = Modifier
         .width(100.dp)
         .height(60.dp)
-        .padding(top = 16.dp)
+        .padding(top = 16.dp, bottom = 16.dp)
         .background(Color.Magenta)
-        .semantics {
+        .semantics(true) {
           contentDescription = "我是大方块"
         }
-    )
+    ) {
+      // 小方块被合并
+      Text(text = "我是小方块")
+    }
+  }
+}
+
+@Composable
+private fun ModifierSemantics02() {
+  Column(modifier = Modifier.padding(16.dp)) {
+    Button(onClick = { /*TODO*/ }) {
+      Text(text = "静夜思", fontWeight = FontWeight(800),
+        modifier = Modifier.semantics(true) {
+
+        })
+    }
+
+    Text(text = "床前明月光，疑是地上霜；")
+    Text(text = "举头望明月，低头思故乡。")
+
+    Box(
+      modifier = Modifier
+        .width(100.dp)
+        .height(60.dp)
+        .padding(top = 16.dp, bottom = 16.dp)
+        .background(Color.Magenta)
+        .clearAndSetSemantics {
+          contentDescription = "我是大方块"
+        }
+    ) {
+      // 小方块不会被读
+      Text(text = "我是小方块")
+    }
   }
 }
