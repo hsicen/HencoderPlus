4c21472daed6857514e342e96fc69a5cff8bd2cf
hsicen <codinghuang@163.com>
2023-07-30 12:04:53 +0800

    [Compose-47] addBefore 和 addAfter 的区别。

---
+++ compose/05_modifier/src/main/java/com/hsicen/a05_modifier/Chapter47.kt
@@ -0,0 +1,27 @@
+package com.hsicen.a05_modifier
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+/******====== 47. addBefore 和 addAfter 的区别 ======******/
+/**
+ * 区别：
+ *  1. addBefore-() 会让里面的 Modifier 身份早于 LayoutModifier 身份被处理，
+ *     导致被包含在 LayoutModifier 所处的 ModifiedLayoutNode 的更内部的一层，
+ *     从而受到更内部的 LayoutNodeWrapper 的尺寸限制。
+ *  2. addAfter-() 会让⾥⾯的 Modifier 身份在 LayoutModifier 身份之后被处理，
+ *     这样就和 LayoutModifier 处于同⼀个 ModifiedLayoutNode，
+ *     从⽽和 LayoutModifier 的尺⼨范围是⼀样的。
+ */
+
+fun ComponentActivity.composeModifier09() {
+  setContent {
+    ModifierAdd01()
+  }
+}
+
+@Composable
+private fun ModifierAdd01() {
+
+}
