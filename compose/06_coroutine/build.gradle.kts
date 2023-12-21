687650b1241595d138d63429f31efc86e31bd224
hsicen <codinghuang@163.com>
2023-12-21 18:35:12 +0800

    [compose-53] Side Effect。

---
+++ compose/06_coroutine/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+  id("comm.app-compose-module")
+}
+
+android {
+  namespace = "com.hsicen.a06_coroutine"
+
+  defaultConfig {
+    applicationId = "com.hsicen.a06_coroutine"
+  }
+}
---
+++ compose/06_coroutine/src/main/java/com/hsicen/a06_coroutine/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hsicen.a06_coroutine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      composeCoroutine01()
+    }
+  }
+}
+
