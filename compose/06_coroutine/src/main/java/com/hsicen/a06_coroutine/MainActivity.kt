1d5b0e3e8531f3de50d981756d474fb97ca6c6c6
hsicen <codinghuang@163.com>
2023-12-23 17:44:35 +0800

    [compose-54] LaunchedEffect 作用和写法。

--- compose/06_coroutine/src/main/java/com/hsicen/a06_coroutine/MainActivity.kt
+++ compose/06_coroutine/src/main/java/com/hsicen/a06_coroutine/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      composeCoroutine02()
+      composeCoroutine03()
     }
   }
 }
