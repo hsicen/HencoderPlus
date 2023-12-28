a2ee2476ae69031faa577e4db8a647381b0425b4
hsicen <codinghuang@163.com>
2023-12-28 17:15:07 +0800

    [compose-60] 定义绘制。

---
+++ compose/07_view/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+  id("comm.app-compose-module")
+}
+
+android {
+  namespace = "com.hsicen.a07_view"
+
+  defaultConfig {
+    applicationId = "com.hsicen.a07_view"
+  }
+}
+
+dependencies {
+  implementation(Deps.liveData)
+}
