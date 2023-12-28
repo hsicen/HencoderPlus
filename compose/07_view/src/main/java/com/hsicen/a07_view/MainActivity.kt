4aef4c44e2590c00c11eda6b47da8d9df9235a31
hsicen <codinghuang@163.com>
2023-12-28 19:23:39 +0800

    [compose-60] ⾃定义绘制。

--- compose/07_view/src/main/java/com/hsicen/a07_view/MainActivity.kt
+++ compose/07_view/src/main/java/com/hsicen/a07_view/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
       HencoderPlusTheme {
         Surface(
           modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colorScheme.background
+          color = MaterialTheme.colors.background
         ) {
           composeView01()
         }
--- compose/07_view/src/main/java/com/hsicen/a07_view/ui/theme/Theme.kt
+++ compose/07_view/src/main/java/com/hsicen/a07_view/ui/theme/Theme.kt
@@ -1,30 +1,26 @@
 package com.hsicen.a07_view.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+private val DarkColorScheme = darkColors(
   primary = Purple80,
-  secondary = PurpleGrey80,
-  tertiary = Pink80
+  primaryVariant = Pink80,
+  secondary = PurpleGrey80
 )
 
-private val LightColorScheme = lightColorScheme(
+private val LightColorScheme = lightColors(
   primary = Purple40,
-  secondary = PurpleGrey40,
-  tertiary = Pink40
+  primaryVariant = Pink40,
+  secondary = PurpleGrey40
 
   /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -40,16 +36,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun HencoderPlusTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {
-    dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-      val context = LocalContext.current
-      if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-    }
-
     darkTheme -> DarkColorScheme
     else -> LightColorScheme
   }
@@ -63,7 +52,7 @@ fun HencoderPlusTheme(
   }
 
   MaterialTheme(
-    colorScheme = colorScheme,
+    colors = colorScheme,
     typography = Typography,
     content = content
   )
