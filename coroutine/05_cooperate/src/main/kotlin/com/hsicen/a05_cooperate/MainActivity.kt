999fb90d0cbb1c0e2c750069dd3b86e80c22be28
hsicen <codinghuang@163.com>
2024-09-12 17:29:52 +0800

    [coroutine-67] 互斥锁和共享变量.

--- coroutine/05_cooperate/src/main/kotlin/com/hsicen/a05_cooperate/MainActivity.kt
+++ coroutine/05_cooperate/src/main/kotlin/com/hsicen/a05_cooperate/MainActivity.kt
@@ -3,10 +3,19 @@ package com.hsicen.a05_cooperate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         setContent {
 
