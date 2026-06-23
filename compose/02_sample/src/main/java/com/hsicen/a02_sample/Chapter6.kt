3aef41691c561b4857d4d81db58723f9df5c8173
hsicen <codinghuang@163.com>
2026-06-23 18:47:14 +0800

    [compose] 1.6 自定义 Composable.

---
+++ compose/02_sample/src/main/java/com/hsicen/a02_sample/Chapter6.kt
@@ -0,0 +1,21 @@
+package com.hsicen.a02_sample
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+
+/******====== 1.6 自定义 Composable ======******/
+
+/**
+ * 1.需要控制布局，不能因为外部因素而改变
+ * 2.在传统View的等价写法 => 自定义View ？
+ *                        => XML布局文件 ？ XML没有逻辑处理
+ *                        => 自定义View + XML 布局文件
+ * 3.对 布局、绘制、触摸 等逻辑进行封装（onDraw、onMeasure、onLayout、onTouchEvent、onInterceptTouchEvent）
+ */
+
+@Composable
+fun ComponentActivity.StateScreen061() {
+
+}
+
+
