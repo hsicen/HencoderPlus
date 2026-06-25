41ec4f710c1ea63423233a0113168f743d4080b5
hsicen <codinghuang@163.com>
2026-06-25 16:39:19 +0800

    [compose] 1.6 自定义 Composable.

--- compose/02_sample/src/main/java/com/hsicen/a02_sample/Chapter6.kt
+++ compose/02_sample/src/main/java/com/hsicen/a02_sample/Chapter6.kt
@@ -1,7 +1,25 @@
 package com.hsicen.a02_sample
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /******====== 1.6 自定义 Composable ======******/
 
@@ -15,6 +33,47 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun ComponentActivity.StateScreen061() {
+    val choices = remember { listOf("名称（A-Z）", "最新安装", "最占空间", "不常用") }
+    val selectItem = remember { mutableIntStateOf(0) }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Text(
+                "你当前选择了：${choices[selectItem.intValue]}",
+                fontSize = 24.sp, color = Color.Red,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        item {
+            Surface(
+                modifier = Modifier
+                    .width(210.dp)
+                    .padding(top = 8.dp),
+                shadowElevation = 2.dp
+            ) {
+                Column {
+                    choices.forEachIndexed { index, choice ->
+                        Text(
+                            choice, fontSize = 18.sp,
+                            color = if (index == selectItem.intValue) Color.Red else Color.Black,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp, bottom = 8.dp)
+                                .clickable { selectItem.intValue = index }
+                        )
+                    }
+                }
+            }
+        }
+    }
 
 }
 
